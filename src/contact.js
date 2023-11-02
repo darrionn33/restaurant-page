5594155083fe020f38ea1195f1e7f811b539b301
darrionn33 <darrionn33@gmail.com>
2023-11-02 22:02:43 +0530

    added slight animation and moved contact h1 outside

--- src/contact.js
+++ src/contact.js
@@ -3,6 +3,7 @@ import put from "put-selector";
 export default function contact(params) {
   const contact = put("main#contact");
   const contactDiv = put("div.contact-div");
+  put(contact, "h1", "Contact Us!");
   put(contact, contactDiv);
 
   const infoDiv = put("div");
@@ -38,7 +39,6 @@ export default function contact(params) {
   put(infoDiv, locationDiv);
   put(infoDiv, timingDiv);
 
-  put(contactDiv, "h1", "Contact Us!");
   put(contactDiv, infoDiv);
   put(contactDiv, "iframe", {
     src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3597.862665181262!2d91.89953234058332!3d25.60947941497087!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x37507f312ba577d1%3A0x793839d4e6c4ac50!2sChill%20Out%20Cafe!5e0!3m2!1sen!2sin!4v1698896482652!5m2!1sen!2sin",
